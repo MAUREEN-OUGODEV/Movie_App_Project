9779867b389168da162916ae1c8bc81ae6971459
Student <ougomaureenakinyi@gmail.com>
2023-08-07 08:40:44 +0300

    Adding Categories button

--- src/components/Hompage/index.jsx
+++ src/components/Hompage/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Display from '../Display'; // Replace with the actual path to Display component
-import GetMovies from '../GetMovies'; // Replace with the actual path to GetMovies component
+import Display from '../Display'; 
+import GetMovies from '../GetMovies'; 
 function HomePage() {
   return (
     <div>
